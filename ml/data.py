6c79f3c98075c69780072aff1ff74fb0f5913f49
Joseph Alberto <jalb107@wgu.edu>
2025-10-17 21:48:41 -0400

    Checking and validating data.py

--- ml/data.py
+++ ml/data.py
@@ -1,7 +1,6 @@
 import numpy as np
 from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
 
-
 def process_data(
     X, categorical_features=[], label=None, training=True, encoder=None, lb=None
 ):
